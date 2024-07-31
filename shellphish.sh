ecd2df2be73b61755f300f9154ea3ec430f67999
Saksham <saksham.23bce10006@vitbhopal.ac.in>
2024-07-31 08:51:22 +0530

    initial commit

--- shellphish.sh
+++ shellphish.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# ShellPhish v2.5-Mod
-# Moded by @AbirHasan2005
 
-# Join Telegram Group for help: http://t.me/linux_repo
 
 
 trap 'printf "\n";stop;exit 1' 2
@@ -77,7 +74,7 @@ printf "\e[1;91m      \ \ | |_  | / _  )| || ||  ____/ | |_  || | /___)| |_  | \
 printf "\e[1;92m  _____) )| | | |( (/ / | || || |      | | | || ||___ || | | | \e[0m\n"
 printf "\e[1;92m (______/ |_| |_| \____)|_||_||_|      |_| |_||_|(___/ |_| |_| \e[0m\n"
 printf "\n"
-printf "\e[1;93m       .:.:.\e[0m\e[1;94m Phishing Tool Moded by @AbirHasan2005 \e[0m\e[1;93m.:.:.\e[0m\n\n"
+printf "\e[1;93m       .:.:.\e[0m\e[1;94m Phishing Tool Moded by @SakshamGupta \e[0m\e[1;93m.:.:.\e[0m\n\n"
 printf "  \e[101m\e[1;77m:: Disclaimer: Developers assume no liability and are not    ::\e[0m\n"
 printf "  \e[101m\e[1;77m:: responsible for any misuse or damage caused by ShellPhish ::\e[0m\n"
 printf " \n"
